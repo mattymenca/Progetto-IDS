de47b45adae2aa5c50068990392d4cf1c3f46c6a
pollo <superplayer292@gmail.com>
2025-09-11 15:56:30 +0200

    Classe dati

--- Implementazione/Model/Gestore.py
+++ Implementazione/Model/Gestore.py
@@ -2,11 +2,26 @@ from StatoOrdine import StatoOrdine
 from Ordine import Ordine
 from Magazzino import Magazzino
 from Prodotto import Prodotto
+import pickle
+
+class Dati:
+    def setOrdini(self, ordini):
+        pass
+    def setContratti(self, contratti):
+        pass
+    def setMagazzino(self, magazzino):
+        pass
+    def setDipendenti(self, dipendenti):
+        pass
+    def setManager(self, manager):
+        pass
+    def salvaTutto(self):
+        pickle.dump(self)
+        pass
 
 class Gestore:
-    
     @staticmethod
-    def validaOrdine(ordine: Ordine, magazzino: Magazzino) -> bool:
+    def approvaOrdine(ordine: Ordine, magazzino: Magazzino) -> bool:
         for prodottoOrdinato in ordine.getProdottiOrdinati():
             nomeProdotto = prodottoOrdinato.getNome()
             if prodottoOrdinato.getQuantita() > magazzino.prodottoDaInventario(nomeProdotto).getQuantita():
