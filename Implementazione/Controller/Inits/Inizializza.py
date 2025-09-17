91c4c419e82176b94c294bb2fd5c8706cf71d95e
pollo <superplayer292@gmail.com>
2025-09-17 15:58:34 +0200

    Ecco

--- Implementazione/Controller/Inits/Inizializza.py
+++ Implementazione/Controller/Inits/Inizializza.py
@@ -2,12 +2,11 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
 
-class Inits:
-    @staticmethod
-    def init():
-        app = QApplication(sys.argv)
-        window = QWidget()
-        window.setWindowTitle("Test")
-        window.setGeometry(100, 100, 800, 600)
-        window.show()
-        sys.exit(app.exec_())
+class Finestra:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.window = QWidget()
+        self.window.setWindowTitle("Test")
+        self.window.setGeometry(100, 100, 800, 600)
+        self.window.show()
+        sys.exit(self.app.exec_())
