e3c9b3c093f326833c30882088a2b33c25503806
pollo <superplayer292@gmail.com>
2025-09-17 15:25:53 +0200

    Basi controller

--- Implementazione/main.py
+++ Implementazione/main.py
@@ -2,13 +2,8 @@
 from Model.Gestore import Gestore, Dati
 from Model.Magazzino.Magazzino import Magazzino
 
-
-def main():
+if __name__ == "__main__":
     dati = Dati()
     dati.caricaDati("File")
     magazzino = Magazzino(dati.prodotti)
     Gestore.creaNuovoProdotto(magazzino, "mela", 5, 1, 0.5, "Tizio", False, 10)
-    
-
-if __name__ == "__main__":
-    main()
