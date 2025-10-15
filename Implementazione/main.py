5a29a41a8b2bb5dd3be65411e42f48c6e459148d
pollo <superplayer292@gmail.com>
2025-10-15 13:05:31 +0200

    Dovrei esserci

--- Implementazione/main.py
+++ Implementazione/main.py
@@ -1,16 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from Model.Dati import Dati
-from Model.GestoreMagazzino import GestoreMagazzino
-from Model.Magazzino.Magazzino import Magazzino
-from Controller.MainController import MainController
+from Controller.Inizio import InizioController
+from Controller.PrimaryController import PrimaryController
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     dati = Dati()
     dati.caricaDati("File")
 
-    controller = MainController()
-    controller.show()
+    manager = PrimaryController()
+    manager.mostraFinestra(InizioController)
 
     sys.exit(app.exec_())
