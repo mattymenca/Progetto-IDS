730a25467dbfd1df0dba036de62a12d0527ccfb5
pollo <superplayer292@gmail.com>
2025-10-15 13:30:06 +0200

    Schermata man/dip

--- Implementazione/Controller/Inizio.py
+++ Implementazione/Controller/Inizio.py
@@ -2,13 +2,6 @@ from PyQt5.QtWidgets import QMainWindow
 from View.inizio import Ui_MainWindow
 # from Controller.Magazzino.MagazzinoController import MagazzinoController 
 
-# Classe fittizia per MagazzinoController per il test
-class MagazzinoController(QMainWindow):
-    def __init__(self, primary_controller):
-        super().__init__()
-        self.primary_controller = primary_controller
-        self.setWindowTitle("Schermata Magazzino")
-
 class InizioController(QMainWindow, Ui_MainWindow):
     # Nota: Il primo parametro è il PrimaryController!
     def __init__(self, primary_controller):
@@ -18,19 +11,3 @@ class InizioController(QMainWindow, Ui_MainWindow):
         # 1. Salva il riferimento al PrimaryController
         self.primary_controller = primary_controller 
         
-        # 2. Connetti i bottoni al metodo di switch
-        self.pushButton.clicked.connect(self.vai_a_magazzino)
-        self.pushButton_2.clicked.connect(self.chiudi_applicazione) # Esempio di chiusura
-        
-    def vai_a_magazzino(self):
-        """
-        Chiede al PrimaryController di gestire lo switch alla schermata Magazzino.
-        """
-        # Si limita a chiamare il manager centralizzato, passandogli la CLASSE di destinazione
-        self.primary_controller.mostraFinestra(MagazzinoController)
-        
-    def chiudi_applicazione(self):
-        """
-        Chiama il metodo per chiudere il controller attuale e permettere al GC di agire.
-        """
-        self.primary_controller.chiudiErimuovi(self)
--- Implementazione/View/inizio.py
+++ Implementazione/View/inizio.py
@@ -1,46 +1,72 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(799, 595)
+        MainWindow.resize(800, 600)
+        
+        # Set a modern, dark stylesheet
+        MainWindow.setStyleSheet("""
+            QMainWindow {
+                background-color: #2c3e50;
+            }
+            QPushButton {
+                background-color: #3498db;
+                color: white;
+                border: none;
+                padding: 15px 30px;
+                text-align: center;
+                text-decoration: none;
+                font-size: 16px;
+                margin: 4px 2px;
+                border-radius: 10px;
+            }
+            QPushButton:hover {
+                background-color: #2980b9;
+            }
+        """)
+        
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 791, 541))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        
+        # Use a central layout to arrange the buttons
+        self.centralLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.centralLayout.setObjectName("centralLayout")
+        self.centralLayout.setContentsMargins(50, 50, 50, 50)
+        self.centralLayout.setSpacing(20)
+
+        # Grid layout for the buttons
+        self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
-        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 0, 1, 1, 1)
-        self.pushButton_4 = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.gridLayout.addWidget(self.pushButton_4, 1, 1, 1, 1)
-        self.pushButton_3 = QtWidgets.QPushButton(self.gridLayoutWidget)
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.setSpacing(20)
+
+        self.pushButton_3 = QtWidgets.QPushButton("Dipendenti", self.centralwidget)
+        self.pushButton_3.setObjectName("pushButton_1")
+        self.pushButton_3.setMinimumSize(QtCore.QSize(150, 50))
         self.gridLayout.addWidget(self.pushButton_3, 1, 0, 1, 1)
+
+        self.pushButton_4 = QtWidgets.QPushButton("Manager", self.centralwidget)
+        self.pushButton_4.setObjectName("pushButton_2")
+        self.pushButton_4.setMinimumSize(QtCore.QSize(150, 50))
+        self.gridLayout.addWidget(self.pushButton_4, 1, 1, 1, 1)
+
+        self.centralLayout.addLayout(self.gridLayout)
+        
         MainWindow.setCentralWidget(self.centralwidget)
+        
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 799, 23))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+        
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Magazzino"))
-        self.pushButton_2.setText(_translate("MainWindow", "Tavoli"))
-        self.pushButton_4.setText(_translate("MainWindow", "Manager"))
-        self.pushButton_3.setText(_translate("MainWindow", "Conti"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Gestionale Ristorante"))
+        # The button text is now set directly in the QPushButton constructor
