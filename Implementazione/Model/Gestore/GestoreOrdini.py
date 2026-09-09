f034fde260fda421e4382c0f4b0a854200ea9dff
mattymenca <matty.menca@gmail.com>
2026-09-09 17:10:20 +0200

    tolti due metodi inutili dentro GestoreOrdini

--- Implementazione/Model/Gestore/GestoreOrdini.py
+++ Implementazione/Model/Gestore/GestoreOrdini.py
@@ -1,27 +1,18 @@
 from Model.Ordine.ordine import Ordine
 from Model.Ordine.StatoOrdine import StatoOrdine
 
-
 class GestoreOrdini:
 
     @staticmethod
-    def _gen_id(dati):
-        return dati.generaNuovoIdOrdine()
-
-    @staticmethod
-    def _set_qta(prodotto, quantita):
-        prodotto.setQuantita(quantita)
-
-    @staticmethod
     def creaOrdine(dati, persona, prodotti_carrello, coperti):
-        id_ordine = GestoreOrdini._gen_id(dati)
+        id_ordine = dati.generaNuovoIdOrdine()
         lista_po = []
 
         for po, prod_originale in prodotti_carrello:
             lista_po.append(po)
 
             nuova_qta = prod_originale.getQuantita() - po.getQuantita()
-            GestoreOrdini._set_qta(prod_originale, nuova_qta)
+            prod_originale.setQuantita(nuova_qta)
 
         nuovo_ordine = Ordine(id_ordine, lista_po, coperti)
         nuovo_ordine.setStatoOrdine(StatoOrdine.IN_CORSO)
@@ -44,7 +35,7 @@ class GestoreOrdini:
 
                     if p.getNomeProdotto() == po.getNome():
                         nuova_qta = p.getQuantita() + po.getQuantita()
-                        GestoreOrdini._set_qta(p, nuova_qta)
+                        p.setQuantita(nuova_qta)
 
             dati.ordini.remove(ordine)
             dati.salvaTutto("dati.pkl")
@@ -57,4 +48,3 @@ class GestoreOrdini:
     def modificaOrdine(dati, ordine_vecchio, persona, nuovi_prodotti_carrello, nuovi_coperti):
         GestoreOrdini.annullaOrdine(dati, ordine_vecchio)
         return GestoreOrdini.creaOrdine(dati, persona, nuovi_prodotti_carrello, nuovi_coperti)
-
